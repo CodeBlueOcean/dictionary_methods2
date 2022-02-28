b9c07a466ddd944db5fce7b55392075bc0ac1cda
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-27 22:32:21 -0600

    Add files via upload

---
+++ dictionary-methods2.py
@@ -0,0 +1,90 @@
+# Dictionary
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+print(user.items())
+
+# Dictionary another example
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+print(user.clear())
+
+
+# Dictionary another example
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+user.clear()
+print(user)
+
+
+# Dictionary another example
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+user2 = user.copy()
+print(user)
+print(user2)
+
+# Dictionary another example
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+user2 = user.copy()
+print(user.clear())
+print(user2)
+
+# Dictionary another example
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+
+print(user.pop('age'))
+print(user)
+
+# Dictionary another example
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+
+print(user.popitem())
+print(user)
+
+# Dictionary another example
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+
+print(user.update({'age': 55}))
+print(user)
+
+# Dictionary another example
+user = {
+    'basket' : [1,2,3],
+    'greet' : 'hello',
+    'age': 20
+}
+
+print(user.update({'ages': 55}))
+print(user)
+
+
+
+
